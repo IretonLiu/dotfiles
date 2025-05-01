598b4803ccc1440824176af73523cbd9cb495120
Ireton Liu <iretonsama@gmail.com>
2025-05-01 16:00:49 +0200

    new hyprland configs

---
+++ setup.sh
@@ -0,0 +1,10 @@
+ln -s $HOME/dotfiles/waybar $HOME/.config
+ln -s $HOME/dotfiles/hypr $HOME/.config
+ln -s $HOME/dotfiles/rofi $HOME/.config
+ln -s $HOME/dotfiles/tmux $HOME/.config
+ln -s $HOME/dotfiles/nvim $HOME/.config
+
+sudo ln -s $HOME/dotfiles/sddm/themes/eucalyptus-drop /usr/share/sddm/themes/
+
+
+
