8ec76d55677a5e6dace5e1484dfd62bf2cb89a88
Ireton <iretonliu@gmail.com>
2025-06-12 21:01:17 +0200

    updated setup script; added plugins to zshrc

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
-ln -s $HOME/dotfiles/waybar $HOME/.config
-ln -s $HOME/dotfiles/hypr $HOME/.config
-ln -s $HOME/dotfiles/rofi $HOME/.config
-ln -s $HOME/dotfiles/tmux $HOME/.config
-ln -s $HOME/dotfiles/nvim $HOME/.config
+ln -sn $HOME/dotfiles/waybar $HOME/.config
+ln -sn $HOME/dotfiles/hypr $HOME/.config
+ln -sn $HOME/dotfiles/rofi $HOME/.config
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -sn $HOME/dotfiles/tmux $HOME/.config
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ln -sn $HOME/dotfiles/nvim $HOME/.config
 
+ln -sn $HOME/dotfiles/alacritty $HOME/.config
 
+ln -sn $HOME/dotfiles/.zshrc $HOME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
